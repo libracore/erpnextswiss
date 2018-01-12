7568588b81ba8953da48e5438572a6c7c52d3bc4
lasalesi <larsericmueller@gmail.com>
2018-01-12 22:23:06 +0100

    implemented Raiffeisen bank import parser

--- erpnextswiss/erpnextswiss/page/bankimport/bankimport.py
+++ erpnextswiss/erpnextswiss/page/bankimport/bankimport.py
@@ -123,6 +123,72 @@ def parse_zkb(content, account, auto_submit=False):
     except IndexError:
         frappe.throw( _("Parsing error. Make sure the correct bank is selected.") )
 
+def parse_raiffeisen(content, account, auto_submit=False):
+    # parse a raiffeisen bank extract csv
+    # collect all lines of the file
+    #log("Starting parser...")
+    lines = content.split("\n")
+    # collect created payment entries
+    new_payment_entries = []
+    try:
+    # if True: # this is for detailed debug messages ;-)
+        for i in range(1, len(lines)):
+            #log("Reading {0} of {1} lines...".format(i, len(lines)))
+            # skip line 0, it contains the column headers
+            # collect each fields (separated by semicolon)
+            fields = lines[i].split(';')
+            
+            # get received amount, only continue if this has a POSITIVE value
+            if len(fields) > 4:
+                if fields[2] != "":
+                    # skip second lines (additional data for payments)
+                    received_amount = float(fields[2])
+                    #log("Received amount {0}".format(received_amount))
+                    if received_amount > 0:
+                        # get unique transaction ID
+                        next_line_fields = lines[i + 1].split(';')
+                        elements_in_comment = next_line_fields[1].split(' ')
+                        if len(elements_in_comment) > 3:
+                            transaction_id = elements_in_comment[-3]
+                        else:
+                            transaction_id = next_line_fields[1]
+                        #log("Checking transaction {0}".format(transaction_id))
+                        # cross-check if this transaction was already recorded
+                        if not frappe.db.exists('Payment Entry', {'reference_no': transaction_id}):
+                            #log("Adding transaction {0}".format(transaction_id))
+                            # create new payment entry
+                            new_payment_entry = frappe.get_doc({'doctype': 'Payment Entry'})
+                            new_payment_entry.payment_type = "Receive"
+                            new_payment_entry.party_type = "Customer";
+                            # get the customer name
+                            description = fields[1].split(" ")
+                            customer_name = " ".join(description[1:])
+                            customer = frappe.get_value('Customer', customer_name, 'name')
+                            if customer:
+                                new_payment_entry.party = customer
+                            else:
+                                new_payment_entry.party = "Guest"
+                            # date is in YYYY-MM-DD
+                            date_time = fields[0].split(' ')
+                            date = date_time[0]
+                            new_payment_entry.posting_date = date
+                            new_payment_entry.paid_to = account
+                            # remove thousands separator
+                            new_payment_entry.received_amount = float(received_amount)
+                            new_payment_entry.paid_amount = float(received_amount)
+                            new_payment_entry.reference_no = transaction_id
+                            new_payment_entry.reference_date = date
+                            if (i + 1) < len(lines):
+                                new_payment_entry.remarks = fields[1] + ", " + next_line_fields[1]
+                            inserted_payment_entry = new_payment_entry.insert()
+                            if auto_submit:
+                                new_payment_entry.submit()
+                            new_payment_entries.append(inserted_payment_entry.name)
+        
+        return new_payment_entries
+    except IndexError:
+        frappe.throw( _("Parsing error. Make sure the correct bank is selected.") )
+        
 def parse_cs(content, account, auto_submit=False):
     # parse a credit suisse bank extract csv
     # collect all lines of the file
@@ -175,6 +241,46 @@ def parse_cs(content, account, auto_submit=False):
         return new_payment_entries
     except IndexError:
         frappe.throw( _("Parsing error. Make sure the correct bank is selected.") )
+
+# this function tries to match the amount to an open sales invoice
+#
+# returns the sales invoice reference (name string) or None
+def match_by_amount(amount):
+    # get sales invoices
+    sql_query = ("SELECT `name` " +
+                "FROM `tabSales Invoice` " +
+                "WHERE `docstatus` = '1 " + 
+                "AND `grand_total` = {0} ".format(amount) + 
+                "AND `status` != 'Paid'")
+    open_sales_invoices = frappe.db.sql(sql_query, as_dict=True)
+    if open_sales_invoices:
+        if len(open_sales_invoices) == 1:
+            # found exactly one match
+            return open_sales_invoices[0].name
+        else:
+            # multiple sales invoices with this amount found
+            return None
+    else:
+        # no open sales invoice with this amount found
+        return None
+        
+# this function tries to match the comments to an open sales invoice
+# 
+# returns the sales invoice reference (name sting) or None
+def match_by_comment(comment):
+    # get sales invoices (submitted, not paid)
+    sql_query = ("SELECT `name` " +
+                "FROM `tabSales Invoice` " +
+                "WHERE `docstatus` = '1 " + 
+                "AND `status` != 'Paid'")
+    open_sales_invoices = frappe.db.sql(sql_query, as_list=True)
+    if open_sales_invoices:
+        # find sales invoice referernce in the comment
+        for reference in open_sales_invoices:
+            if reference in comment:
+                # found a match
+                return reference
+    return None
      
 def log(comment):
 	new_comment = frappe.get_doc({"doctype": "Log"})
@@ -206,6 +312,8 @@ def parse_file(content, bank, account, auto_submit=False):
         new_records = parse_ubs(content, account, auto_submit)
     elif bank == "zkb":
         new_records = parse_zkb(content, account, auto_submit)
+    elif bank == "raiffeisen":
+        new_records = parse_raiffeisen(content, account, auto_submit)
     elif bank == "cs":
         new_records = parse_cs(content, account, auto_submit)
                

f6562600cb77258af04f87601edbe5c29f0953ad
Joel Mesmer <joel.mesmer@libracore.com>
2018-02-08 09:17:43 +0100

    resolved issue #14

--- erpnextswiss/scripts/item_tools.py
+++ erpnextswiss/scripts/item_tools.py
@@ -13,17 +13,22 @@ import frappe
 import re
 
 @frappe.whitelist()
-def get_next_item_code():
+def get_next_item_code(start_value="1"):
 	#This function is used to automatically create a new item code that is 1 higher than the last one created.
 	
 	#get last created Item Code
 	last_item_code = frappe.db.sql("SELECT item_code FROM tabItem ORDER BY creation DESC LIMIT 1", as_dict=True)
+
+	#Check if already an item exist
+	if last_item_code:
+		#Get numeric part of item code for increase with regex
+		pattern = re.compile("(\D*)(\d+)")
+		last_item_code_clean = pattern.findall((last_item_code[0]["item_code"]))
+		
+		#increase numeric item code with 1 and add prefix (if avaible)
+		next_item_code = str(last_item_code_clean[0][0]) + str(int(last_item_code_clean[0][1]) + 1)
 	
-	#Get numeric part of item code for increase with regex
-	pattern = re.compile("(\w*)(\d+)")
-	last_item_code_clean = pattern.findall((last_item_code[0]["item_code"]))
-	
-	#increase numeric item code with 1 and add prefix (if avaible)
-	next_item_code = str(last_item_code_clean[-1][0]) + str(int(last_item_code_clean[-1][1]) + 1)
-	
-	return next_item_code
+		return next_item_code
+		
+	#if its the first item, return start value
+	return start_value

48ae05032c06754d4dd7145f2c14a43aa9872314
lasalesi <larsericmueller@gmail.com>
2018-02-19 11:35:54 +0100

    automation functions and translation

--- erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.js
+++ erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.js
@@ -3,6 +3,47 @@
 
 frappe.ui.form.on('Salary Certificate', {
 	refresh: function(frm) {
-
+        // add utility buttons
+        frm.add_custom_button(__("Get values"), function() {
+            if (frm.doc.name.startsWith("New")) {
+                frappe.msgprint( __("Please save the record before populating it with data") );
+            } else {
+                frappe.call({
+                    method: 'fetch_values',
+                    doc: frm.doc,
+                    callback: function(response) {
+                        refresh_field(['salary', 'gross_salary', 'net_salary', 'deduction_ahv', 'deduction_pk']);
+                                    
+                        // show a short information
+                        show_alert( __("Salary slip information collected"));
+                    }
+                }); 
+            }
+        });
+        frm.add_custom_button(__("Calculate totals"), function() {
+            // calculate gross salary
+            var gross_salary = frm.doc.salary
+                + frm.doc.catering
+                + frm.doc.car
+                + frm.doc.other_salary
+                + frm.doc.irregular
+                + frm.doc.capital
+                + frm.doc.participation
+                + frm.doc.board
+                + frm.doc.other;
+            
+            // calculate net salary
+            var net_salary = gross_salary 
+                - frm.doc.deduction_ahv
+                - frm.doc.deduction_pension
+                - frm.doc.deduction_pension_additional;
+                
+            // assign to form
+            cur_frm.set_value('gross_salary', gross_salary);
+            cur_frm.set_value('net_salary', net_salary);    
+            
+            // show a short information
+            show_alert( __("Totals have been updated"));
+        });
 	}
 });
--- erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.py
+++ erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.py
@@ -7,4 +7,33 @@ import frappe
 from frappe.model.document import Document
 
 class SalaryCertificate(Document):
-	pass
+    # this function gathers values from the database
+    def fetch_values(self):
+        if not self.employee:
+            frappe.msgprint( _("Please select a valid employee.") )
+            return
+        
+        sql_query = """SELECT IFNULL(SUM(`gross_pay`), 0) AS `gross`, IFNULL(SUM(`net_pay`), 0) AS `net` FROM `tabSalary Slip` 
+            WHERE `employee` = '{0}'
+            AND `posting_date` >= '{1}'
+            AND `posting_date` <= '{2}'""".format(self.employee, self.start_date, self.end_date)
+        values = frappe.db.sql(sql_query, as_dict=True)
+        self.salary = get_component(self.employee, self.start_date, self.end_date, "B")
+        self.gross_salary = values[0].gross
+        self.net_salary = values[0].net
+        self.deduction_ahv = get_component(self.employee, self.start_date, self.end_date, "AHV")
+        self.deduction_pension = get_component(self.employee, self.start_date, self.end_date, "PK")   
+            
+        self.save()
+        return
+    
+def get_component(employee, start_date, end_date, component):
+    sql_query = """SELECT `employee`, IFNULL(SUM(`tabSalary Detail`.`amount`), 0) AS `amount`
+        FROM `tabSalary Slip` 
+        INNER JOIN `tabSalary Detail` ON `tabSalary Slip`.`name` = `tabSalary Detail`.`parent`
+        WHERE `employee` = '{0}'
+        AND `posting_date` >= '{1}'
+        AND `posting_date` <= '{2}'
+        AND `abbr` = '{3}';""".format(employee, start_date, end_date, component)
+    values = frappe.db.sql(sql_query, as_dict=True)
+    return values[0].amount

c0578a65e322ddb139253f9e9a3c14ce5c432141
lasalesi <larsericmueller@gmail.com>
2018-06-13 15:55:31 +0200

    using common script

--- erpnextswiss/docs/custom_scripts/purchase_invoice.js
+++ erpnextswiss/docs/custom_scripts/purchase_invoice.js
@@ -1,7 +1,8 @@
 frappe.ui.form.on('Purchase Invoice', {
   validate: function(frm) {
     if (frm.doc.payment_type == "ESR") {
-      if (!check_esr(frm.doc.esr_reference_number)) {
+      frappe.provide("erpnextswiss.swiss_common");
+      if (!erpnextswiss.swiss_common.check_esr(frm.doc.esr_reference_number)) {
         frappe.msgprint( __("ESR code not valid") ); 
         frappe.validated=false;
       } 
@@ -31,27 +32,3 @@ frappe.ui.form.on('Purchase Invoice', {
     }
   }
 });
-
-function check_esr(esr_raw) {
-  esr_code = esr_raw.replace(/ /g, '');
-  // define algo
-  alg = new Array (0,9,4,6,8,2,7,1,3,5);
-  // convert esr code into array form
-  esr_array = esr_code.split("");
-  // preset remainder R2
-  R2 = 0;
-  // loop through digites except check digit
-  for ( i = 0; i < (esr_code.length - 1); i++ ) {
-    // get remainder plus next digit
-    Rpr = parseInt(R2) + parseInt(esr_array[i]);
-    // modulo 10 from algo
-    R2 = alg[Rpr % 10];
-  }
-  // check digit
-  P2 = (10 - R2) % 10;
-  if (parseInt(esr_array[esr_array.length - 1]) == P2) {
-    return true;
-  } else {
-    return false;
-  }
-}

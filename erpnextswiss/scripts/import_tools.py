1db94c500aa5d43108e452c5dbf1e4af53293f2d
lasalesi <larsericmueller@gmail.com>
2018-07-08 19:32:45 +0000

    script snippets for import

--- erpnextswiss/scripts/import_tools.py
+++ erpnextswiss/scripts/import_tools.py
@@ -19,6 +19,8 @@ ART_BEST_NR=6   	# Artikelnummer für Lieferanten
 ART_BEZ=7       	# Artikelname
 ART_NR1=68		# Artikelgruppe (Hauptgruppe)
 ART_NR2=69		# Artikelgruppe (Untergruppe)
+ART_NR3=70
+ART_NR4=71
 ART_BEST_NR=6		# Artikelnummer für Lieferanten
 
 ART_ANZST1=8		# Materialien:Edelstein (1. Zeile)  > Anzahl
@@ -53,7 +55,7 @@ ART_EKDAT=50		# Letztes Einkaufsdatum (?)
 ART_VKDAT=51		# Letztes Verkaufsdatum (?)
 ART_STK=52		# Anfangsbestand
 ART_TOT=54		# Total Verkauft (?)
-
+LIE_MCODE=72		# brand code
 
 def import_items(filename):
     # read input file
@@ -74,6 +76,15 @@ def import_items(filename):
 	    try:
 		doc = frappe.get_doc("Item", get_field(cells[ART_NR]))
 		update = True
+		doc.cm_art_nr = "{0}.{1}.{2}.{3}".format(
+			get_field(cells[ART_NR1]),
+			get_field(cells[ART_NR2]),
+			get_field(cells[ART_NR3]),
+			get_field(cells[ART_NR4]))
+		doc.basismat = "{0}".format(get_field(cells[ART_NR3]))
+		doc.liemar = "{0}".format(get_field(cells[LIE_MCODE]))
+		doc.save()
+		print("Updated {0}".format(get_field(cells[ART_NR])))
 	    except:
 		# find supplier
 		suppliers = frappe.get_all('Supplier', filters={'supplier_number': get_field(cells[ART_LIE_COD2])}, fields=['name'])	
@@ -153,8 +164,11 @@ def import_items(filename):
 		    })
 		# insert record
 		print("{0}".format(doc.item_group))
-		doc.insert()
-		print("Inserted " + get_field(cells[ART_NR]))
+		try:
+			doc.insert()
+			print("Inserted " + get_field(cells[ART_NR]))
+		except:
+			print("Inserting " + get_field(cells[ART_NR]) + " failed")
     return
 
 # removes the quotation marks from a cell
@@ -177,3 +191,172 @@ def test():
 		
 	})
     doc.insert()
+
+# loops through all items and applies the basis mat to the subtable
+def apply_basismat():
+	items = frappe.get_all('Item', filters={'disabled': 0}, fields=['name', 'basismat'])
+	lookup = {
+		'0': 'Edelstahl',
+		'1': 'Stahl/Lederband',
+		'2': 'Stahl/Stahlband',
+		'3': 'Bicolor',
+		'4': 'Bicolor/Lederband',
+		'5': 'Bicolor/Metallband',
+		'6': 'Stahl titanisiert',
+		'7': 'Stahl-PVD-beschichtet',
+		'8': 'Stahl mit 750 Gelbgold',
+		'10': 'Titan',
+		'11': 'Titan/Lederband',
+		'12': 'Titan/Metallband',
+		'13': 'Titan/Bicolor',
+		'14': 'Titan/Bicolor/Lederband',
+		'15': 'Titan/Bicolor/Metallband',
+		'16': 'Titan/750 Gelbgold',
+		'17': 'Diamant',
+		'18': 'Carbon',
+		'19': 'Zinn',
+		'20': 'Plaqué',
+		'21': 'Plaqué/Lederband',
+		'22': 'Plaqué/Metallband',
+		'23': 'Amerikaner',
+		'24': 'vergoldet',
+		'25': 'Nickel',
+		'26': 'verchromt',
+		'27': 'vernickelt',
+		'28': 'rhodiniert',
+		'29': 'versilbert',
+		'30': '333 GG',
+		'31': '333 WG',
+		'32': '333 RG',
+		'33': '333 bi',
+		'34': 'Glas',
+		'35': '375 GG',
+		'36': '375 WG',
+		'37': '375 RG',
+		'38': '375 bicolor',
+		'39': '375 Tri',
+		'40': 'Hartmetall kratzfest',
+		'41': 'Hartmetall/Lederband',
+		'42': 'Hartmetall/Metallband',
+		'43': 'H\'metall/H\'metallband',
+		'44': 'H\'metall Bicolor',
+		'45': 'H\'metall Bicolor/Lederbd',
+		'46': 'H\'metall Bicolor/Met\'b.',
+		'47': 'Messing',
+		'48': 'Metall-Lackiert',
+		'49': 'Leichtmetall',
+		'50': '750 GG/Kautschuk',
+		'51': '585 GG/Goldband',
+		'52': '585 WG/Lederband',
+		'53': '585 WG/Goldband',
+		'54': 'Metall antiallergisch',
+		'55': '585 GG',
+		'56': '585 WG',
+		'57': '585 RG',
+		'58': '585 bi',
+		'59': '585 Tri',
+		'60': 'Keramik',
+		'61': 'Keramik/Lederband',
+		'62': 'Keramik/Metallband',
+		'63': 'Keramik/Keramikband',
+		'64': 'Porzellan',
+		'65': 'Naturschiefer',
+		'66': 'Kunststoff',
+		'67': 'K\'stoff/K\'stoffband',
+		'68': 'K\'stoff/Metallband',
+		'69': 'Stahl/Gummiband',
+		'70': '750 GG/Lederband',
+		'71': '750 GG/Goldband',
+		'72': '750 WG/Lederband',
+		'73': '750 WG/Goldband',
+		'74': '750 rh',
+		'75': '750 GG',
+		'76': '750 WG',
+		'77': '750 RG',
+		'78': '750 bicolor',
+		'79': '750 tricolor',
+		'80': '800 Silber',
+		'81': '800 Silber vergoldet',
+		'82': '800 Silber goldplattiert',
+		'83': '800 Silber rh',
+		'84': 'Zuchtperlen',
+		'85': 'Süsswasserperlen',
+		'86': 'Imit. Perlen',
+		'87': 'Koralle',
+		'88': '800 Silber/Lederband',
+		'89': '800 Silber/Silberband',
+		'90': '925 Silber',
+		'91': '925 Silber vergoldet',
+		'92': '925 Silber/Gold 750',
+		'93': '925 Silber rh',
+		'94': 'Palladium',
+		'95': '950 Platin',
+		'96': '950 Platin mit GG',
+		'97': 'Holz',
+		'98': '999.9 Gold',
+		'99': '925 Silber/Silberband'
+	}
+	# loop through all active items
+	for item in items:
+	  print('.'),
+	  # if there is a basismat, check child table
+	  if item.basismat:
+	    print('-'),
+	    details = frappe.get_all('Item Detail', filters={'parent': item.name, 'element': 'Basismat'}, fields=['name'])
+	    # prevent crashes due to missing entries
+	    try:
+	      # load all Basismat child entries
+	      details = frappe.get_all('Item Detail', filters={'parent': item.name, 'element': 'Basismat'}, fields=['name'])
+	      # if a child entry was found, load it
+	      if details:
+		print('>'),
+		detail = frappe.get_doc('Item Detail', details[0]['name'])
+		# if the detail contains no material, load it
+		if not detail.material:
+			print('o'),
+			detail.material = lookup[item.basismat]
+			detail.save()
+			detail.submit()
+			print("Item {0} updated with {1}".format(item.name, detail.material))
+	    except:
+		# do nothing
+		pass
+
+def correct_weights(filename):
+    # read input file
+    file = open(filename, "rU")
+    data = file.read().decode('utf-8')
+    rows = data.split(ROW_SEPARATOR)
+    print("Rows: {0}".format(len(rows)))
+    for i in range(1, len(rows)):
+        cells = rows[i].split(CELL_SEPARATOR)
+
+        if len(cells) > 1:
+            print("Cells: {0}".format(len(cells)))
+            # check if item exists
+            print("Checking " + get_field(cells[ART_NR]))
+            update_detail(get_field(cells[ART_NR]), "Edelstein", get_field(cells[ART_BEZ1]), get_field(cells[ART_GEW1]))
+            update_detail(get_field(cells[ART_NR]), "Edelstein", get_field(cells[ART_BEZ2]), get_field(cells[ART_GEW2]))
+            update_detail(get_field(cells[ART_NR]), "Edelstein", get_field(cells[ART_BEZ3]), get_field(cells[ART_GEW3]))
+            update_detail(get_field(cells[ART_NR]), "Edelstein", get_field(cells[ART_BEZ4]), get_field(cells[ART_GEW4]))
+    return
+
+def update_detail(item_code, element, material, weight):
+	matches = frappe.get_all('Item Detail', filters={'parent': item_code, 'element': element, 'material': material}, fields=['name'])
+	if matches:
+		doc = frappe.get_doc('Item Detail', matches[0]['name'])
+		doc.weight = weight
+		doc.save()
+		print("Updated {0} {1} {2}".format(item_code, element, material))
+	return
+
+# read the length from the CM items
+def read_length():
+	matches = frappe.get_all('Item Detail', filters={'element': "Edelstein", 'material': "CM"}, fields=['parent', 'count'])
+	if matches:
+		for match in matches:
+			doc = frappe.get_doc("Item", match['parent'])
+			doc.length = match['count']
+			doc.save()
+			print("Updated {0} with {1} cm".format(match['parent'], match['count']))
+	return

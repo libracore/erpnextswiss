8d9444b945db5d7c9a727205534a741c1170d698
lasalesi <larsericmueller@gmail.com>
2018-08-02 22:23:15 +0200

    query based on GL entry

--- erpnextswiss/erpnextswiss/page/abacus_export/abacus_export.py
+++ erpnextswiss/erpnextswiss/page/abacus_export/abacus_export.py
@@ -10,219 +10,98 @@ from frappe import throw, _
 def generate_transfer_file(start_date, end_date):
     # creates a transfer file for abacus
 
-    #try:
-        # convert JavaScript parameter into Python array
-        payments = eval(payments)
-        # remove empty items in case there should be any (bigfix for issue #2)
-        payments = filter(None, payments)
-        
-        # array for skipped payments
-        skipped = []
-        
+    #try:        
         # create xml header
         content = make_line("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
-        # define xml template reference
-        content += make_line("<Document xmlns=\"http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd  pain.001.001.03.ch.02.xsd\">")
-        # transaction holder
-        content += make_line("  <CstmrCdtTrfInitn>")
-        ### Group Header (GrpHdr, A-Level)
-        # create group header
-        content += make_line("    <GrpHdr>")
-        # message ID (unique, SWIFT-characters only)
-        content += make_line("      <MsgId>MSG-" + time.strftime("%Y%m%d%H%M%S") + "</MsgId>")
-        # creation date and time ( e.g. 2010-02-15T07:30:00 )
-        content += make_line("      <CreDtTm>" + time.strftime("%Y-%m-%dT%H:%M:%S") + "</CreDtTm>")
-        # number of transactions in the file
+        # define xml root node
+        content += make_line("<AbaConnectContainer>")
+        # control counter (TaskCount is actually the number of transactions)
         transaction_count = 0
         transaction_count_identifier = "<!-- $COUNT -->"
-        content += make_line("      <NbOfTxs>" + transaction_count_identifier + "</NbOfTxs>")
-        # total amount of all transactions ( e.g. 15850.00 )  (sum of all amounts)
-        control_sum = 0.0
-        control_sum_identifier = "<!-- $CONTROL_SUM -->"
-        content += make_line("      <CtrlSum>" + control_sum_identifier + "</CtrlSum>")
-        # initiating party requires at least name or identification
-        content += make_line("      <InitgPty>")
-        # initiating party name ( e.g. MUSTER AG )
-        content += make_line("        <Nm>" + get_company_name(payments[0]) + "</Nm>")
-        content += make_line("      </InitgPty>")
-        content += make_line("    </GrpHdr>")
-        
-        ### Payment Information (PmtInf, B-Level)
-        # payment information records (1 .. 99'999)
-        for payment in payments:
-            # create temporary code block to compile the payment record (only add to overall on submit)
-            # use 'continue' to skip a record (in case a validation fails)
-            payment_content = ""
-            # retrieve the payment entry record
-            payment_record = frappe.get_doc('Payment Entry', payment)
-            # create the payment entries
-            payment_content += make_line("    <PmtInf>")
-            # unique (in this file) identification for the payment ( e.g. PMTINF-01, PMTINF-PE-00005 )
-            payment_content += make_line("      <PmtInfId>PMTINF-" + payment + "</PmtInfId>")
-            # payment method (TRF or TRA, no impact in Switzerland)
-            payment_content += make_line("      <PmtMtd>TRF</PmtMtd>")
-            # batch booking (true or false; recommended true)
-            payment_content += make_line("      <BtchBookg>true</BtchBookg>")
-            # Requested Execution Date (e.g. 2010-02-22)
-            payment_content += make_line("      <ReqdExctnDt>{0}</ReqdExctnDt>".format(
-                payment_record.posting_date))
-            # debitor (technically ignored, but recommended)   
-            payment_content += make_line("      <Dbtr>")
-            # debitor name
-            payment_content += make_line("        <Nm>" +
-                payment_record.company + "</Nm>")
-            # postal address (recommendadtion: do not use)
-            #content += make_line("        <PstlAdr>")
-            #content += make_line("          <Ctry>CH</Ctry>")
-            #content += make_line("          <AdrLine>SELDWYLA</AdrLine>")
-            #content += make_line("        </PstlAdr>")
-            payment_content += make_line("      </Dbtr>")
-            # debitor account (sender) - IBAN
-            payment_account = frappe.get_doc('Account', payment_record.paid_from)
-            payment_content += make_line("      <DbtrAcct>")
-            payment_content += make_line("        <Id>")
-            if payment_account.iban:
-                payment_content += make_line("          <IBAN>{0}</IBAN>".format(
-                    payment_account.iban.replace(" ", "") ))
-            else:
-                # no paying account IBAN: not valid record, skip
-                content += add_invalid_remark( _("{0}: no account IBAN found ({1})".format(
-                    payment, payment_record.paid_from) ) )
-                skipped.append(payment)
-                continue
-            payment_content += make_line("        </Id>")
-            payment_content += make_line("      </DbtrAcct>")
-            if payment_account.bic:
-                # debitor agent (sender) - BIC
-                payment_content += make_line("      <DbtrAgt>")
-                payment_content += make_line("        <FinInstnId>")
-                payment_content += make_line("          <BIC>" +
-                    payment_account.bic + "</BIC>")
-                payment_content += make_line("        </FinInstnId>")
-                payment_content += make_line("      </DbtrAgt>")
-                
-            ### Credit Transfer Transaction Information (CdtTrfTxInf, C-Level)
-            payment_content += make_line("      <CdtTrfTxInf>")
-            # payment identification
-            payment_content += make_line("        <PmtId>")
-            # instruction identification 
-            payment_content += make_line("          <InstrId>INSTRID-" + payment + "</InstrId>")
-            # end-to-end identification (should be used and unique within B-level; payment entry name)
-            payment_content += make_line("          <EndToEndId>" + payment + "</EndToEndId>")
-            payment_content += make_line("        </PmtId>")
-            # payment type information
-            payment_content += make_line("        <PmtTpInf>")
-            # service level: only used for SEPA (currently not implemented)
-            if payment_record.transaction_type == "SEPA":
-                payment_content += make_line("          <SvcLvl>")
-                # service level code (e.g. SEPA)
-                payment_content += make_line("            <Cd>SEPA</Cd>")
-                payment_content += make_line("          </SvcLvl>")
-            # local instrument
-            if payment_record.transaction_type == "ESR":
-                payment_content += make_line("          <LclInstrm>")
-                # proprietary (nothing or CH01 for ESR)        
-                payment_content += make_line("            <Prtry>CH01</Prtry>")
-                payment_content += make_line("          </LclInstrm>")        
-            payment_content += make_line("        </PmtTpInf>")
-            # amount 
-            payment_content += make_line("        <Amt>")
-            payment_content += make_line("          <InstdAmt Ccy=\"{0}\">{1:.2f}</InstdAmt>".format(
-                payment_record.paid_from_account_currency,
-                payment_record.paid_amount))
-            payment_content += make_line("        </Amt>")
-            # creditor account
-            # creditor account identification
-            if payment_record.transaction_type == "ESR":
-                # add creditor information
-                creditor_info = add_creditor_info(payment_record)
-                if creditor_info:
-                    payment_content += creditor_info
-                else:
-                    # no address found, skip entry (not valid)
-                    content += add_invalid_remark( _("{0}: no address (or country) found").format(payment) )
-                    skipped.append(payment)
-                    continue
-                # ESR payment
-                payment_content += make_line("        <CdtrAcct>")
-                payment_content += make_line("          <Id>")
-                payment_content += make_line("            <Othr>")
-                # ESR participant number
-                if payment_record.esr_participant_number:
-                    payment_content += make_line("              <Id>" +
-                        payment_record.esr_participant_number + "</Id>")
-                else:
-                    # no particpiation number: not valid record, skip
-                    content += add_invalid_remark( _("{0}: no ESR participation number found").format(payment) )
-                    skipped.append(payment)
-                    continue
-                payment_content += make_line("            </Othr>")
-                payment_content += make_line("          </Id>")
-                payment_content += make_line("        </CdtrAcct>")
-                # Remittance Information
-                payment_content += make_line("        <RmtInf>")
-                payment_content += make_line("          <Strd>")
-                # Creditor Reference Information
-                payment_content += make_line("            <CdtrRefInf>")
-                # ESR reference 
-                if payment_record.esr_reference:
-                    payment_content += make_line("              <Ref>" +
-                        payment_record.esr_reference.replace(" ", "") + "</Ref>")
-                else:
-                    # no ESR reference: not valid record, skip
-                    content += add_invalid_remark( _("{0}: no ESR reference found").format(payment) )
-                    skipped.append(payment)
-                    continue    
-                payment_content += make_line("            </CdtrRefInf>")
-                payment_content += make_line("          </Strd>")
-                payment_content += make_line("        </RmtInf>")
-            else:
-                # IBAN or SEPA payment
-                # add creditor information
-                creditor_info = add_creditor_info(payment_record)
-                if creditor_info:
-                    payment_content += creditor_info
-                else:
-                    # no address found, skip entry (not valid)
-                    content += add_invalid_remark( _("{0}: no address (or country) found").format(payment) )
-                    skipped.append(payment)
-                    continue
-                # creditor agent (BIC, optional; removed to resolve issue #15)
-                #if payment_record.bic:                
-                #    payment_content += make_line("        <CdtrAgt>")
-                #    payment_content += make_line("          <FinInstnId>")
-                #    payment_content += make_line("            <BIC>" + 
-                #        payment_record.bic + "</BIC>")
-                #    payment_content += make_line("          </FinInstnId>")
-                #    payment_content += make_line("        </CdtrAgt>")    
-                # creditor account
-                payment_content += make_line("        <CdtrAcct>")
-                payment_content += make_line("          <Id>")
-                if payment_record.iban:
-                    payment_content += make_line("            <IBAN>{0}</IBAN>".format( 
-                        payment_record.iban.replace(" ", "") ))
-                else:
-                    # no iban: not valid record, skip
-                    content += add_invalid_remark( _("{0}: no IBAN found").format(payment) )
-                    skipped.append(payment)
-                    continue
-                payment_content += make_line("          </Id>")
-                payment_content += make_line("        </CdtrAcct>")
-                                        
-            # close payment record
-            payment_content += make_line("      </CdtTrfTxInf>")
-            payment_content += make_line("    </PmtInf>")
-            # once the payment is extracted for payment, submit the record
+        content += make_line(" <TaskCount>{0}</TaskCount>".format(transaction_count_identifier))
+        # task container
+        content += make_line(" <Task>")
+        # parameters
+        content += make_line("  <Parameter>")
+        content += make_line("   <Application>FIBU</Application>")
+        content += make_line("   <Id>XML Buchungen</Id>")
+        content += make_line("   <MapId>AbaDefault</MapId>")
+        content += make_line("   <Version>2015.00</Version>")
+        content += make_line("  </Parameter>")
+        # add transactions
+        sql_query = """SELECT 
+                `tabGL Entry`.`posting_date` AS `posting_date`, 
+                `tabGL Entry`.`debit` AS `debit`, 
+                `tabGL Entry`.`credit` AS `credit`, 
+                `tabGL Entry`.`account` AS `account`, 
+                `tabGL Entry`.`account_currency` AS `account_currency`,
+                `tabAccount`.`account_number` AS `account_number`,
+                `tabGL Entry`.`voucher_type` AS `voucher_type`, 
+                `tabGL Entry`.`voucher_no` AS `voucher_no`
+            FROM `tabGL Entry`
+            LEFT JOIN `tabAccount` ON `tabGL Entry`.`account` = `tabAccount`.`name`
+            WHERE
+            	`posting_date` >= '{start_date}'
+				AND `posting_date` <= '{end_date}'
+				AND `docstatus` = 1
+            """.format(start_date=start_date, end_date=end_date)
+        items = frappe.db.sql(sql_query, as_dict=True)
+        for item in items:
             transaction_count += 1
-            control_sum += payment_record.paid_amount
-            content += payment_content
-            payment_record.submit()
+            content += make_line("  <Transaction id=\"{0}\">").format(transaction_count)
+            content += make_line("   <Entry mode=\"SAVE\">")
+            content += make_line("    <CollectiveInformation mode=\"SAVE\">")
+            content += make_line("     <EntryLevel>A</EntryLevel>")
+            content += make_line("     <EntryType>S</EntryType>")
+            content += make_line("     <Type>Normal</Type>")
+            if item.debit:
+                code = "D"
+                value = item.debit
+            else:
+                code = "C"
+                value = item.credit
+            content += make_line("     <DebitCredit>{0}</DebitCredit>").format(code)
+            content += make_line("     <Client></Client>")
+            content += make_line("     <Division>0</Division>")
+            content += make_line("     <KeyCurrency>{0}</KeyCurrency>").format(item.account_currency)
+            content += make_line("     <EntryDate>{0}</EntryDate>").format(item.posting_date)
+            content += make_line("     <ValueDate></ValueDate>")
+            content += make_line("     <AmountData mode=\"SAVE\">")
+            content += make_line("       <Currency>{0}</Currency>").format(item.account_currency)
+            content += make_line("       <Amount>{0}</Amount>").format(value)
+            content += make_line("     </AmountData>")
+            content += make_line("     <KeyAmount>{0}</KeyAmount>").format(value)
+            content += make_line("     <Account>{0}</Account>").format(item.account_number)
+            content += make_line("     <IntercompanyId>0</IntercompanyId>")
+            content += make_line("     <IntercompanyCode></IntercompanyCode>")
+            content += make_line("     <Text1>Sammelbuchung</Text1>")
+            content += make_line("     <DocumentNumber>{0}</DocumentNumber>").format(item.voucher_no)
+            content += make_line("     <SingleCount>0</SingleCount>")
+            content += make_line("    </CollectiveInformation>")
+            content += make_line("    <SingleInformation mode=\"SAVE\">")
+            content += make_line("      <Type>Normal</Type>")
+            content += make_line("      <DebitCredit>{0}</DebitCredit>").format(code)
+            content += make_line("      <EntryDate>{0}</EntryDate>").format(item.posting_date)
+            content += make_line("      <ValueDate></ValueDate>")
+            content += make_line("      <AmountData mode=\"SAVE\">")
+            content += make_line("        <Currency>{0}</Currency>").format(item.account_currency)
+            content += make_line("        <Amount>{0}</Amount>").format(value)
+            content += make_line("      </AmountData>")
+            content += make_line("      <KeyAmount>{0}</KeyAmount>").format(value)
+            content += make_line("      <Account>2280</Account>")
+            content += make_line("      <IntercompanyId>0</IntercompanyId>")
+            content += make_line("      <IntercompanyCode></IntercompanyCode>")
+            content += make_line("      <Text1>Sammelbuchung</Text1>")
+            content += make_line("      <DocumentNumber>49377</DocumentNumber>")
+            content += make_line("      <SelectionCode></SelectionCode>")
+            content += make_line("    </SingleInformation>")
+            content += make_line("   </Entry>")
+            content += make_line("  </Transaction>")
         # add footer
-        content += make_line("  </CstmrCdtTrfInitn>")
-        content += make_line("</Document>")
+        content += make_line(" </Task>")
+        content += make_line("</AbaConnectContainer>")
         # insert control numbers
         content = content.replace(transaction_count_identifier, "{0}".format(transaction_count))
-        content = content.replace(control_sum_identifier, "{:.2f}".format(control_sum))
         
         return { 'content': content }
     #except IndexError:
@@ -231,3 +110,7 @@ def generate_transfer_file(start_date, end_date):
     #except:
     #    frappe.throw( _("Error while generating xml. Make sure that you made required customisations to the DocTypes.") )
     #    return
+
+# adds Windows-compatible line endings (to make the xml look nice)    
+def make_line(line):
+    return line + "\r\n"

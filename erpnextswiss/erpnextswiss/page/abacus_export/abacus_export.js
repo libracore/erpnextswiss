036dac628f645f5fc23aa1fc5229b84813ff28a3
lasalesi <larsericmueller@gmail.com>
2018-08-02 21:19:46 +0200

    gui implemented

--- erpnextswiss/erpnextswiss/page/abacus_export/abacus_export.js
+++ erpnextswiss/erpnextswiss/page/abacus_export/abacus_export.js
@@ -1,12 +1,13 @@
 frappe.pages['abacus_export'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: __('Abacus Export'),
-		single_column: true
-	});
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: __('Abacus Export'),
+        single_column: true
+    });
 
-	frappe.abacus_export.make(page);
-	frappe.abacus_export.run();
+    frappe.abacus_export.make(page);
+    frappe.abacus_export.run();
+    frappe.abacus_export.update_preview();
     
     // add the application reference
     frappe.breadcrumbs.add("ERPNextSwiss");
@@ -14,43 +15,111 @@ frappe.pages['abacus_export'].on_page_load = function(wrapper) {
 
 
 frappe.abacus_export = {
-	start: 0,
-	make: function(page) {
-		var me = frappe.abacus_export;
-		me.page = page;
-		me.body = $('<div></div>').appendTo(me.page.main);
-		var data = "";
-		$(frappe.render_template('abacus_export', data)).appendTo(me.body);
+    start: 0,
+    make: function(page) {
+        var me = frappe.abacus_export;
+        me.page = page;
+        me.body = $('<div></div>').appendTo(me.page.main);
+        var data = "";
+        $(frappe.render_template('abacus_export', data)).appendTo(me.body);
         
-		// attach button handlers
-		this.page.main.find(".btn-create-file").on('click', function() {
-			var me = frappe.abacus_export;
+        // attach button handlers
+        this.page.main.find(".btn-create-file").on('click', function() {
+            var me = frappe.abacus_export;
 
 
+        });
+    },
+    run: function() {
+        // set beginning of the year as start and today as current date
+        var today = new Date();
+        var dd = today.getDate();
+        if (dd < 10) { dd = "0" + dd; }
+        var mm = today.getMonth() + 1;     //January is 0!
+        if (mm < 10) { mm = "0" + mm; }
+        var yyyy = today.getFullYear();
+        var input_start = document.getElementById("start_date");
+        input_start.value = yyyy + "-01-01";
+        var input_end = document.getElementById("end_date");
+        input_end.value = yyyy + "-" + mm + "-" + dd;
+        
+        // attach change handlers
+        input_start.onchange = function() { frappe.abacus_export.update_preview(); };
+        input_end.onchange = function() { frappe.abacus_export.update_preview(); };
+    },
+    update_preview: function() {
+		// get date values
+		start_date = document.getElementById("start_date").value;
+		end_date = document.getElementById("end_date").value;
+		if (end_date < start_date) {
+			// switch values
+			document.getElementById("start_date").value = end_date;
+			document.getElementById("end_date").value = start_date;
+			var temp = start_date;
+			start_date = end_date;
+			end_date = temp;
+		}
+        // get GL entries from this period
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+				doctype:"GL Entry",
+				filters: [
+					["posting_date",">=", start_date],
+					["posting_date","<=", end_date],
+					["docstatus","=", 1]
+				],
+                fields: ["posting_date", "debit", "credit", "account", "voucher_type", "voucher_no"],
+                order_by: "posting_date"
+            },
+			callback: function(response) {
+				var preview_container = document.getElementById("preview");
+				preview_container.innerHTML = "";
+                if (response.message) {
+                    if (response.message.length > 0) {
+                        preview_container.innerHTML += frappe.render_template('gl_entry_table', response);
+                    } 
+                    if (response.message.length == 20) {
+						preview_container.innerHTML += '<p class="text-muted">' + __("more records available (not shown)") + '</p>';
+					}
+                } else {
+					preview_container.innerHTML += '<p class="text-muted">' + __("No general ledger entries found.") + '</p>';
+				}
+            }
+        });
+    },
+    create_transfer_file: function() {
+		// enable waiting gif
+		document.getElementById("waiting-gif").classList.remove("hide");
+		// get date range
+		start_date = document.getElementById("start_date").value;
+		end_date = document.getElementById("end_date").value;
+		// generate payment file
+		frappe.call({
+			method: 'erpnextswiss.erpnextswiss.page.abacus_export.abacus_export.generate_transfer_file',
+			args: { 
+				'start_date': start_date,
+				'end_date': end_date
+			},
+			callback: function(r) {
+				if (r.message) {
+					// prepare the xml file for download
+					frappe.abacus_export.download("transfer.xml", r.message.content);
+					
+					// disable waiting gif
+					document.getElementById("waiting-gif").classList.add("hide");
+				} 
+			}
 		});
 	},
-	run: function() {
-		// set beginning of the year as start and today as current date
-		var today = new Date();
-		var dd = today.getDate();
-		if (dd < 10) { dd = "0" + dd; }
-		var mm = today.getMonth() + 1; 	//January is 0!
-		if (mm < 10) { mm = "0" + mm; }
-		var yyyy = today.getFullYear();
-		var input_start = document.getElementById("start_date");
-		input_start.value = yyyy + "-01-01";
-		var input_end = document.getElementById("end_date");
-		input_end.value = yyyy + "-" + mm + "-" + dd;
-		
-		// attach change handlers
-		input_start.onchange = function() { frappe.abacus_export.update_preview(); };
-		input_end.onchange = function() { frappe.abacus_export.update_preview(); };
-	},
-	update_preview: function() {
-		console.log("here...");
-		preview_container = document.getElementById("preview");
-		preview_container.innerHTML = "";
-		preview_container.innerHTML += "Preview!";
+    download: function (filename, content) {
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:application/octet-stream;charset=utf-8,' + encodeURIComponent(content));
+		element.setAttribute('download', filename);
+		element.style.display = 'none';
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
 	}
 }
 
--- erpnextswiss/erpnextswiss/page/abacus_export/abacus_export.py
+++ erpnextswiss/erpnextswiss/page/abacus_export/abacus_export.py
@@ -5,3 +5,229 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import throw, _
+
+@frappe.whitelist()
+def generate_transfer_file(start_date, end_date):
+    # creates a transfer file for abacus
+
+    #try:
+        # convert JavaScript parameter into Python array
+        payments = eval(payments)
+        # remove empty items in case there should be any (bigfix for issue #2)
+        payments = filter(None, payments)
+        
+        # array for skipped payments
+        skipped = []
+        
+        # create xml header
+        content = make_line("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+        # define xml template reference
+        content += make_line("<Document xmlns=\"http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd  pain.001.001.03.ch.02.xsd\">")
+        # transaction holder
+        content += make_line("  <CstmrCdtTrfInitn>")
+        ### Group Header (GrpHdr, A-Level)
+        # create group header
+        content += make_line("    <GrpHdr>")
+        # message ID (unique, SWIFT-characters only)
+        content += make_line("      <MsgId>MSG-" + time.strftime("%Y%m%d%H%M%S") + "</MsgId>")
+        # creation date and time ( e.g. 2010-02-15T07:30:00 )
+        content += make_line("      <CreDtTm>" + time.strftime("%Y-%m-%dT%H:%M:%S") + "</CreDtTm>")
+        # number of transactions in the file
+        transaction_count = 0
+        transaction_count_identifier = "<!-- $COUNT -->"
+        content += make_line("      <NbOfTxs>" + transaction_count_identifier + "</NbOfTxs>")
+        # total amount of all transactions ( e.g. 15850.00 )  (sum of all amounts)
+        control_sum = 0.0
+        control_sum_identifier = "<!-- $CONTROL_SUM -->"
+        content += make_line("      <CtrlSum>" + control_sum_identifier + "</CtrlSum>")
+        # initiating party requires at least name or identification
+        content += make_line("      <InitgPty>")
+        # initiating party name ( e.g. MUSTER AG )
+        content += make_line("        <Nm>" + get_company_name(payments[0]) + "</Nm>")
+        content += make_line("      </InitgPty>")
+        content += make_line("    </GrpHdr>")
+        
+        ### Payment Information (PmtInf, B-Level)
+        # payment information records (1 .. 99'999)
+        for payment in payments:
+            # create temporary code block to compile the payment record (only add to overall on submit)
+            # use 'continue' to skip a record (in case a validation fails)
+            payment_content = ""
+            # retrieve the payment entry record
+            payment_record = frappe.get_doc('Payment Entry', payment)
+            # create the payment entries
+            payment_content += make_line("    <PmtInf>")
+            # unique (in this file) identification for the payment ( e.g. PMTINF-01, PMTINF-PE-00005 )
+            payment_content += make_line("      <PmtInfId>PMTINF-" + payment + "</PmtInfId>")
+            # payment method (TRF or TRA, no impact in Switzerland)
+            payment_content += make_line("      <PmtMtd>TRF</PmtMtd>")
+            # batch booking (true or false; recommended true)
+            payment_content += make_line("      <BtchBookg>true</BtchBookg>")
+            # Requested Execution Date (e.g. 2010-02-22)
+            payment_content += make_line("      <ReqdExctnDt>{0}</ReqdExctnDt>".format(
+                payment_record.posting_date))
+            # debitor (technically ignored, but recommended)   
+            payment_content += make_line("      <Dbtr>")
+            # debitor name
+            payment_content += make_line("        <Nm>" +
+                payment_record.company + "</Nm>")
+            # postal address (recommendadtion: do not use)
+            #content += make_line("        <PstlAdr>")
+            #content += make_line("          <Ctry>CH</Ctry>")
+            #content += make_line("          <AdrLine>SELDWYLA</AdrLine>")
+            #content += make_line("        </PstlAdr>")
+            payment_content += make_line("      </Dbtr>")
+            # debitor account (sender) - IBAN
+            payment_account = frappe.get_doc('Account', payment_record.paid_from)
+            payment_content += make_line("      <DbtrAcct>")
+            payment_content += make_line("        <Id>")
+            if payment_account.iban:
+                payment_content += make_line("          <IBAN>{0}</IBAN>".format(
+                    payment_account.iban.replace(" ", "") ))
+            else:
+                # no paying account IBAN: not valid record, skip
+                content += add_invalid_remark( _("{0}: no account IBAN found ({1})".format(
+                    payment, payment_record.paid_from) ) )
+                skipped.append(payment)
+                continue
+            payment_content += make_line("        </Id>")
+            payment_content += make_line("      </DbtrAcct>")
+            if payment_account.bic:
+                # debitor agent (sender) - BIC
+                payment_content += make_line("      <DbtrAgt>")
+                payment_content += make_line("        <FinInstnId>")
+                payment_content += make_line("          <BIC>" +
+                    payment_account.bic + "</BIC>")
+                payment_content += make_line("        </FinInstnId>")
+                payment_content += make_line("      </DbtrAgt>")
+                
+            ### Credit Transfer Transaction Information (CdtTrfTxInf, C-Level)
+            payment_content += make_line("      <CdtTrfTxInf>")
+            # payment identification
+            payment_content += make_line("        <PmtId>")
+            # instruction identification 
+            payment_content += make_line("          <InstrId>INSTRID-" + payment + "</InstrId>")
+            # end-to-end identification (should be used and unique within B-level; payment entry name)
+            payment_content += make_line("          <EndToEndId>" + payment + "</EndToEndId>")
+            payment_content += make_line("        </PmtId>")
+            # payment type information
+            payment_content += make_line("        <PmtTpInf>")
+            # service level: only used for SEPA (currently not implemented)
+            if payment_record.transaction_type == "SEPA":
+                payment_content += make_line("          <SvcLvl>")
+                # service level code (e.g. SEPA)
+                payment_content += make_line("            <Cd>SEPA</Cd>")
+                payment_content += make_line("          </SvcLvl>")
+            # local instrument
+            if payment_record.transaction_type == "ESR":
+                payment_content += make_line("          <LclInstrm>")
+                # proprietary (nothing or CH01 for ESR)        
+                payment_content += make_line("            <Prtry>CH01</Prtry>")
+                payment_content += make_line("          </LclInstrm>")        
+            payment_content += make_line("        </PmtTpInf>")
+            # amount 
+            payment_content += make_line("        <Amt>")
+            payment_content += make_line("          <InstdAmt Ccy=\"{0}\">{1:.2f}</InstdAmt>".format(
+                payment_record.paid_from_account_currency,
+                payment_record.paid_amount))
+            payment_content += make_line("        </Amt>")
+            # creditor account
+            # creditor account identification
+            if payment_record.transaction_type == "ESR":
+                # add creditor information
+                creditor_info = add_creditor_info(payment_record)
+                if creditor_info:
+                    payment_content += creditor_info
+                else:
+                    # no address found, skip entry (not valid)
+                    content += add_invalid_remark( _("{0}: no address (or country) found").format(payment) )
+                    skipped.append(payment)
+                    continue
+                # ESR payment
+                payment_content += make_line("        <CdtrAcct>")
+                payment_content += make_line("          <Id>")
+                payment_content += make_line("            <Othr>")
+                # ESR participant number
+                if payment_record.esr_participant_number:
+                    payment_content += make_line("              <Id>" +
+                        payment_record.esr_participant_number + "</Id>")
+                else:
+                    # no particpiation number: not valid record, skip
+                    content += add_invalid_remark( _("{0}: no ESR participation number found").format(payment) )
+                    skipped.append(payment)
+                    continue
+                payment_content += make_line("            </Othr>")
+                payment_content += make_line("          </Id>")
+                payment_content += make_line("        </CdtrAcct>")
+                # Remittance Information
+                payment_content += make_line("        <RmtInf>")
+                payment_content += make_line("          <Strd>")
+                # Creditor Reference Information
+                payment_content += make_line("            <CdtrRefInf>")
+                # ESR reference 
+                if payment_record.esr_reference:
+                    payment_content += make_line("              <Ref>" +
+                        payment_record.esr_reference.replace(" ", "") + "</Ref>")
+                else:
+                    # no ESR reference: not valid record, skip
+                    content += add_invalid_remark( _("{0}: no ESR reference found").format(payment) )
+                    skipped.append(payment)
+                    continue    
+                payment_content += make_line("            </CdtrRefInf>")
+                payment_content += make_line("          </Strd>")
+                payment_content += make_line("        </RmtInf>")
+            else:
+                # IBAN or SEPA payment
+                # add creditor information
+                creditor_info = add_creditor_info(payment_record)
+                if creditor_info:
+                    payment_content += creditor_info
+                else:
+                    # no address found, skip entry (not valid)
+                    content += add_invalid_remark( _("{0}: no address (or country) found").format(payment) )
+                    skipped.append(payment)
+                    continue
+                # creditor agent (BIC, optional; removed to resolve issue #15)
+                #if payment_record.bic:                
+                #    payment_content += make_line("        <CdtrAgt>")
+                #    payment_content += make_line("          <FinInstnId>")
+                #    payment_content += make_line("            <BIC>" + 
+                #        payment_record.bic + "</BIC>")
+                #    payment_content += make_line("          </FinInstnId>")
+                #    payment_content += make_line("        </CdtrAgt>")    
+                # creditor account
+                payment_content += make_line("        <CdtrAcct>")
+                payment_content += make_line("          <Id>")
+                if payment_record.iban:
+                    payment_content += make_line("            <IBAN>{0}</IBAN>".format( 
+                        payment_record.iban.replace(" ", "") ))
+                else:
+                    # no iban: not valid record, skip
+                    content += add_invalid_remark( _("{0}: no IBAN found").format(payment) )
+                    skipped.append(payment)
+                    continue
+                payment_content += make_line("          </Id>")
+                payment_content += make_line("        </CdtrAcct>")
+                                        
+            # close payment record
+            payment_content += make_line("      </CdtTrfTxInf>")
+            payment_content += make_line("    </PmtInf>")
+            # once the payment is extracted for payment, submit the record
+            transaction_count += 1
+            control_sum += payment_record.paid_amount
+            content += payment_content
+            payment_record.submit()
+        # add footer
+        content += make_line("  </CstmrCdtTrfInitn>")
+        content += make_line("</Document>")
+        # insert control numbers
+        content = content.replace(transaction_count_identifier, "{0}".format(transaction_count))
+        content = content.replace(control_sum_identifier, "{:.2f}".format(control_sum))
+        
+        return { 'content': content }
+    #except IndexError:
+    #    frappe.msgprint( _("Please select at least one payment."), _("Information") )
+    #    return
+    #except:
+    #    frappe.throw( _("Error while generating xml. Make sure that you made required customisations to the DocTypes.") )
+    #    return

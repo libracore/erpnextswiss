7d8c15209da3a296a7b3dbab3df4da07224ea89d
lasalesi <larsericmueller@gmail.com>
2018-09-16 14:09:17 +0200

    extending POS works

--- erpnextswiss/public/js/pos_swiss_extensions.js
+++ erpnextswiss/public/js/pos_swiss_extensions.js
@@ -1,30 +1,8 @@
 console.log("Loading POS extensions...");
 
-/* frappe.provide('erpnext.pos');
-
 frappe.pages['pos'].refresh = function (wrapper) {
-    console.log(wrapper);
-    frappe.msgprint( __("Hello World") ); 
-    
-    wrapper.pos.frm.page.add_menu_item( __("Greeting"), function() {
-       frappe.msgprint( __("Hello World") ); 
-    }); 
-    console.log(wrapper.pos); // undefined 
-    
-}
-
-frm.page.add_menu_item( __("Greeting"), function() {
-   frappe.msgprint( __("Hello World") ); 
-}); */
-
-
-
-frappe.pages['pos'].refresh = function (wrapper) {
-    var action_bar = document.getElementsByClassName('page-actions');
-    var newButton = document.createElement("div");
-    if (action_bar.length > 0) {
-        // action_bar[0].addChild(newButton); // throws an error
-        action_bar[0].innerHTML += "<button class=\"btn btn-primary\"><span class=\"hidden-xs\">ERPNextSwiss</span></button>"; // this will override all buttons
-        console.log("button added");
-    }
+    // add entries to action menu
+    cur_page.page.page.add_action_item("ERPNextSwiss", function() {
+        frappe.msgprint("ERPNextSwiss!");
+    });    
 }

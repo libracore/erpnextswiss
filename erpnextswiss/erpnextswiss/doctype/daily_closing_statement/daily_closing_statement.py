f938dfb5979a8180e351d2ba39aeb47072ddc0d0
lasalesi <larsericmueller@gmail.com>
2019-01-26 21:50:14 +0100

    all statistics and translations

--- erpnextswiss/erpnextswiss/doctype/daily_closing_statement/daily_closing_statement.py
+++ erpnextswiss/erpnextswiss/doctype/daily_closing_statement/daily_closing_statement.py
@@ -22,7 +22,7 @@ class DailyClosingStatement(Document):
               `tabSales Invoice Item`.`item_group` AS `item_group`,
               `tabSales Taxes and Charges`.`rate` AS `tax_rate`, 
               `tabSales Invoice Item`.`amount` AS `amount`,
-              ROUND(IF(`tabSales Taxes and Charges`.`rate` IS NULL, 1, 1 + (`tabSales Taxes and Charges`.`rate` / 100)) * `tabSales Invoice Item`.`amount`, 2) AS `gross`
+              ROUND(IF(`tabSales Taxes and Charges`.`rate` IS NULL, 1, 1 + (`tabSales Taxes and Charges`.`rate` / 100)) * `tabSales Invoice Item`.`amount`, 5) AS `gross`
             FROM `tabSales Invoice Item`
             LEFT JOIN `tabSales Taxes and Charges` ON `tabSales Invoice Item`. `parent` = `tabSales Taxes and Charges`.`parent`
             LEFT JOIN `tabSales Invoice` ON `tabSales Invoice Item`.`parent` = `tabSales Invoice`.`name`
@@ -32,15 +32,6 @@ class DailyClosingStatement(Document):
         
         self.items = []
         for result in results:
-            """self.items.append({
-                'parent': self.name,
-                'doctype': 'Daily Closing Statement Item',
-                'parentfield': "items",
-                'parenttype': "Daily Closing Statement",
-                'item_code': result['item'],
-                'sales_invoice': result['sinv'],
-                'amount': result['gross']
-            })"""
             row = self.append('items', {
                 'item_code': result['item'], 
                 'item_name': result['item_name'],
@@ -48,6 +39,99 @@ class DailyClosingStatement(Document):
                 'sales_invoice': result['sinv'], 
                 'amount': result['gross']
             })
+        
+        # total sales
+        sql_query = """SELECT SUM(`tabSales Invoice`.`base_grand_total`) AS `base_grand_total`
+            FROM `tabSales Invoice`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}";
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        self.total_sales = results[0]['base_grand_total']
+        # number of customers 
+        sql_query = """SELECT COUNT(`tabSales Invoice`.`customer`) AS `customer_count`
+            FROM `tabSales Invoice`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}
+            GROUP BY `tabSales Invoice`.`customer`";
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        self.number_of_customers = results[0]['customer_count']        
+        # average & highest sales
+        sql_query = """SELECT AVG(`tabSales Invoice`.`base_grand_total`) AS `avg`,
+              MAX(`tabSales Invoice`.`base_grand_total`) AS `max`
+            FROM `tabSales Invoice`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}";
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        self.average_sales = results[0]['avg']
+        self.highest_sale = results[0]['max']
+        # discounts
+        sql_query = """SELECT SUM(`tabSales Invoice`.`base_discount_amount`) AS `discounts`
+            FROM `tabSales Invoice`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}
+            GROUP BY `tabSales Invoice`.`customer`";
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        self.discounts = results[0]['discounts']  
+        
+        # by groups
+        sql_query = """SELECT 
+              COUNT(`tabSales Invoice Item`.`item_code`) AS `count`,
+              `tabSales Invoice Item`.`item_group` AS `item_group`,
+              `tabSales Taxes and Charges`.`rate` AS `tax_rate`, 
+              SUM(`tabSales Invoice Item`.`amount`) AS `amount`,
+              SUM(ROUND(IF(`tabSales Taxes and Charges`.`rate` IS NULL, 1, 1 + (`tabSales Taxes and Charges`.`rate` / 100)) * `tabSales Invoice Item`.`amount`, 5)) AS `gross`
+            FROM `tabSales Invoice Item`
+            LEFT JOIN `tabSales Taxes and Charges` ON `tabSales Invoice Item`. `parent` = `tabSales Taxes and Charges`.`parent`
+            LEFT JOIN `tabSales Invoice` ON `tabSales Invoice Item`.`parent` = `tabSales Invoice`.`name`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}"
+            GROUP BY `tabSales Invoice Item`.`item_group`;
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        
+        self.groups = []
+        for result in results:
+            row = self.append('groups', {
+                'item_group': result['item_group'],
+                'count': result['count'], 
+                'amount': result['gross']
+            })
             
+        # by currency
+        sql_query = """SELECT 
+              COUNT(`tabSales Invoice`.`name`) AS `count`,
+              `tabSales Invoice`.`currency` AS `currency`,
+              SUM(`tabSales Invoice`.`base_grand_total`) AS `amount`
+            FROM `tabSales Invoice`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}"
+            GROUP BY `tabSales Invoice`.`currency`;
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        
+        self.currencies = []
+        for result in results:
+            row = self.append('currencies', {
+                'currency': result['currency'],
+                'count': result['count'], 
+                'amount': result['amount']
+            })
             
+        # by payment mode
+        sql_query = """SELECT 
+              COUNT(`tabSales Invoice Payment`.`name`) AS `count`,
+              `tabSales Invoice Payment`.`mode_of_payment` AS `mode_of_payment`,
+              SUM(`tabSales Invoice Payment`.`base_amount`) AS `amount`
+            FROM `tabSales Invoice Payment`
+            LEFT JOIN `tabSales Invoice` ON `tabSales Invoice Payment`.`parent` = `tabSales Invoice`.`name`
+            WHERE `tabSales Invoice`.`posting_date` >= "{start_date}" AND `tabSales Invoice`.`posting_date` <= "{end_date}"
+            GROUP BY `tabSales Invoice Payment`.`mode_of_payment`;
+        """.format(start_date=self.start_date, end_date=self.end_date)
+        results = frappe.db.sql(sql_query, as_dict=True)
+        
+        self.payment_modes = []
+        for result in results:
+            row = self.append('payment_modes', {
+                'mode_of_payment': result['mode_of_payment'],
+                'count': result['count'], 
+                'amount': result['amount']
+            })
 	pass

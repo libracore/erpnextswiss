5b18899e2d8e77b7cfe568856ea09c569f52c7cf
lasalesi <larsericmueller@gmail.com>
2019-09-05 14:51:35 +0200

    removing fullsize, has been merged to core in v12

--- erpnextswiss/hooks.py
+++ erpnextswiss/hooks.py
@@ -17,8 +17,8 @@ app_license = "AGPL"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/erpnextswiss/css/erpnextswiss.css"
 app_include_js = [
-    "/assets/erpnextswiss/js/swiss_common.js",
-    "/assets/erpnextswiss/js/fullsize.js"]
+    "/assets/erpnextswiss/js/swiss_common.js"
+    ]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/erpnextswiss/css/erpnextswiss.css"

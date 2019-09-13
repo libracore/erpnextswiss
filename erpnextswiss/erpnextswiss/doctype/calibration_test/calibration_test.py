860db4471303dabe2af609b9bf1c9ceaaea2f5a5
Joelios <joel.mesmer@libracore.com>
2019-09-13 14:18:04 +0200

    preparing for presentation

--- erpnextswiss/erpnextswiss/doctype/calibration_test/calibration_test.py
+++ erpnextswiss/erpnextswiss/doctype/calibration_test/calibration_test.py
@@ -5,6 +5,8 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils.data import nowdate, getdate
+from frappe import _
 
 class CalibrationTest(Document):
 	def validate(self):
@@ -21,3 +23,22 @@ class CalibrationTest(Document):
 				row.actual_value = test.actual_value
 				row.remarks = test.remarks
 				row.inspection_decision_ok = test.inspection_decision_ok
+				
+	def on_submit(self):
+		result = True
+		for test in self.test_plan_items:
+			if test.inspection_decision_ok == 0:
+				result = False
+				
+		
+		inspection_equipment = frappe.get_doc("Inspection Equipment", self.inspection_equipment)
+		inspection_equipment.last_calibration = getdate(nowdate())
+		#test passed
+		if result:
+			inspection_equipment.status = 'Calibrated'
+		#test failed
+		else:
+			inspection_equipment.status = 'To Calibrate'
+			
+		inspection_equipment.save()
+		

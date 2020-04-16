ff256d2bbdfa82f78c3719038778bab7cd58e6e1
lasalesi <larsericmueller@gmail.com>
2020-04-16 22:00:54 +0200

    select primary address and contact

--- erpnextswiss/scripts/crm_tools.py
+++ erpnextswiss/scripts/crm_tools.py
@@ -2,7 +2,7 @@
 #
 # crm_tools.py
 #
-# Copyright (C) libracore, 2017-2018
+# Copyright (C) libracore, 2017-2020
 # https://www.libracore.com or https://github.com/libracore
 #
 # For information on ERPNext, refer to https://erpnext.org/
@@ -24,10 +24,46 @@ def get_customer_address(customer):
         return address
     else:
         return None
-    
+
+# fetch the primary available address from a customer
+@frappe.whitelist()
+def get_primary_customer_address(customer):
+    sql_query = u"""SELECT `tabDynamic Link`.`parent`, `tabAddress`.`is_primary_address`
+            FROM `tabDynamic Link` 
+            LEFT JOIN `tabAddress` ON `tabAddress`.`name` = `tabDynamic Link`.`parent`
+            WHERE  `tabDynamic Link`.`link_doctype` = "Customer"
+                   AND `tabDynamic Link`.`link_name` = "{customer}"
+                   AND `tabDynamic Link`.`parenttype` = "Address"
+            ORDER BY `tabAddress`.`is_primary_address` DESC;
+        """.format(customer=customer)
+    address_name = frappe.db.sql(sql_query, as_dict=True)
+    if address_name:
+        address = frappe.get_doc("Address", address_name[0]['parent'])
+        return address
+    else:
+        return None
+        
 # fetch the first available contact from a customer
 @frappe.whitelist()
 def get_customer_contact(customer):
+    sql_query = u"""SELECT `tabDynamic Link`.`parent`, `tabContact`.`is_primary_contact`
+            FROM `tabDynamic Link` 
+            LEFT JOIN `tabContact` ON `tabContact`.`name` = `tabDynamic Link`.`parent`
+            WHERE  `tabDynamic Link`.`link_doctype` = "Customer"
+                   AND `tabDynamic Link`.`link_name` = "{customer}"
+                   AND `tabDynamic Link`.`parenttype` = "Contact"
+            ORDER BY `tabContact`.`is_primary_contact` DESC;
+        """.format(customer=customer)
+    contact_name = frappe.db.sql(sql_query, as_dict=True)
+    if contact_name:
+        contact = frappe.get_doc("Contact", contact_name[0]['parent'])
+        return contact
+    else:
+        return None
+
+# fetch the primary available contact from a customer
+@frappe.whitelist()
+def get_primary_customer_contact(customer):
     sql_query = u"""SELECT `parent` FROM `tabDynamic Link` WHERE
         `link_doctype` = "Customer"
         AND `link_name` = "{customer}"
@@ -39,7 +75,7 @@ def get_customer_contact(customer):
         return contact
     else:
         return None
-
+        
 # fetch the first available address from a customer
 @frappe.whitelist()
 def get_supplier_address(supplier):

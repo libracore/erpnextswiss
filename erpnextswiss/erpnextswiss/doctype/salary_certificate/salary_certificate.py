2db7b2eacac653c8e5b67869be3fbe218bd64729
lasalesi <larsericmueller@gmail.com>
2021-01-20 20:52:01 +0100

    improved full reference

--- erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.py
+++ erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.py
@@ -13,12 +13,12 @@ class SalaryCertificate(Document):
             frappe.msgprint( _("Please select a valid employee.") )
             return
         
-        sql_query = """SELECT IFNULL(SUM(`gross_pay`), 0) AS `gross`, IFNULL(SUM(`net_pay`), 0) AS `net` 
+        sql_query = """SELECT IFNULL(SUM(`tabSalary Slip`.`gross_pay`), 0) AS `gross`, IFNULL(SUM(`tabSalary Slip`.`net_pay`), 0) AS `net` 
             FROM `tabSalary Slip` 
-            WHERE `employee` = '{0}'
-            AND `posting_date` >= '{1}'
-            AND `posting_date` <= '{2}'
-            AND `docstatus` = 1;""".format(self.employee, self.start_date, self.end_date)
+            WHERE `tabSalary Slip`.`employee` = '{0}'
+            AND `tabSalary Slip`.`posting_date` >= '{1}'
+            AND `tabSalary Slip`.`posting_date` <= '{2}'
+            AND `tabSalary Slip`.`docstatus` = 1;""".format(self.employee, self.start_date, self.end_date)
         values = frappe.db.sql(sql_query, as_dict=True)
         self.salary = get_component(self.employee, self.start_date, self.end_date, "B")
         self.gross_salary = values[0].gross
@@ -32,13 +32,13 @@ class SalaryCertificate(Document):
         return
     
 def get_component(employee, start_date, end_date, component):
-    sql_query = """SELECT `employee`, IFNULL(SUM(`tabSalary Detail`.`amount`), 0) AS `amount`
+    sql_query = """SELECT `tabSalary Slip`.`employee`, IFNULL(SUM(`tabSalary Detail`.`amount`), 0) AS `amount`
         FROM `tabSalary Slip` 
         INNER JOIN `tabSalary Detail` ON `tabSalary Slip`.`name` = `tabSalary Detail`.`parent`
-        WHERE `employee` = '{0}'
-        AND `posting_date` >= '{1}'
-        AND `posting_date` <= '{2}'
-        AND `docstatus` = 1
-        AND `abbr` = '{3}';""".format(employee, start_date, end_date, component)
+        WHERE `tabSalary Slip`.`employee` = '{0}'
+        AND `tabSalary Slip`.`posting_date` >= '{1}'
+        AND `tabSalary Slip`.`posting_date` <= '{2}'
+        AND `tabSalary Slip`.`docstatus` = 1
+        AND `tabSalary Detail`.`abbr` = '{3}';""".format(employee, start_date, end_date, component)
     values = frappe.db.sql(sql_query, as_dict=True)
     return values[0].amount

377d872859a657721f208faeaf5edadc55ad0b53
lasalesi <larsericmueller@gmail.com>
2021-05-20 09:59:27 +0200

    open map from pincode form

--- erpnextswiss/erpnextswiss/doctype/pincode/pincode.js
+++ erpnextswiss/erpnextswiss/doctype/pincode/pincode.js
@@ -3,7 +3,20 @@
 
 frappe.ui.form.on('Pincode', {
     refresh: function(frm) {
-
+        frm.add_custom_button( __("Open Map") , function() {
+            frappe.call({
+                "method": "erpnextswiss.erpnextswiss.swisstopo.get_swisstopo_url_from_pincode",
+                "args": {
+                    "pincode": frm.doc.pincode,
+                    "language": "de",
+                    "zoom": 8
+                },
+                "callback": function(response) {
+                    var url = response.message;
+                    window.open(url, '_blank');
+                }
+            });
+        });
     },
     validate: function(frm) {
         cur_frm.set_value('title', frm.doc.pincode + "-" + frm.doc.city);

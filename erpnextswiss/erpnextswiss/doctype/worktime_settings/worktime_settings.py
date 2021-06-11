b7bbb766a1330abdd4c53b5f8e8900d9dcc53b97
lasalesi <larsericmueller@gmail.com>
2021-06-11 21:40:47 +0200

    allow to directly query working hours

--- erpnextswiss/erpnextswiss/doctype/worktime_settings/worktime_settings.py
+++ erpnextswiss/erpnextswiss/doctype/worktime_settings/worktime_settings.py
@@ -3,8 +3,20 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class WorktimeSettings(Document):
-	pass
+    pass
+
+@frappe.whitelist()
+def get_daily_working_hours(company=None):
+    if not company:
+        company = frappe.defaults.get_global_default('company')
+    hours = frappe.db.sql("""SELECT `daily_hours`
+                     FROM `tabDaily Hours` 
+                     WHERE `company` = "{c}";""".format(c=company), as_dict=True)
+    if hours and len(hours) > 0:
+        return hours[0]['daily_hours']
+    else:
+        return 8

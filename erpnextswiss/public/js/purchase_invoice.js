cb59ed53bb50adfdd416434828a69622e5fd20a3
lasalesi <larsericmueller@gmail.com>
2021-07-23 07:23:28 +0200

    adding esr check

--- erpnextswiss/public/js/purchase_invoice.js
+++ erpnextswiss/public/js/purchase_invoice.js
@@ -6,7 +6,40 @@ frappe.ui.form.on('Purchase Invoice', {
                 check_defaults(frm);
             });
         }
+    },
+    validate: function(frm) {
+        if (frm.doc.payment_type == "ESR") {
+          if (!check_esr(frm.doc.esr_reference_number)) {
+            frappe.msgprint( __("ESR code not valid") ); 
+            frappe.validated=false;
+          } 
+        }
+
+        if ((frm.doc.supplier) && (frm.doc.bill_no)) {
+          frappe.call({
+            method:"frappe.client.get_list",
+            args:{
+              doctype: "Purchase Invoice",		
+              filters: {
+                'supplier': frm.doc.supplier,
+                'bill_no': frm.doc.bill_no
+              },		
+              fields: ['name'],
+              async: false
+            },
+            callback: function(r) {
+              r.message.forEach(function(pinv) { 
+                if (pinv.name != frm.doc.name) {
+                  frappe.msgprint(  __("This invoice is already recorded in ") + pinv.name );
+                  frappe.validated=false;
+                }
+              });
+            }
+          });     
+        }
     }
+});
+
 });
 
 function check_defaults(frm) {

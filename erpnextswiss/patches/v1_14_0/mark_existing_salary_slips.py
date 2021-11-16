a3a365674fa3f4e2d6838bc818c9ea779bb6e633
lasalesi <larsericmueller@gmail.com>
2021-11-16 17:27:47 +0100

    hotfix: frappe migration order runs patch before customsation (dead-lock)

--- erpnextswiss/patches/v1_14_0/mark_existing_salary_slips.py
+++ erpnextswiss/patches/v1_14_0/mark_existing_salary_slips.py
@@ -3,7 +3,10 @@ from frappe import _
 
 def execute():
     # this will mark all existing salary slips as proposed and prevent, that they will go into a payment run
-    frappe.db.sql("""UPDATE `tabSalary Slip` SET `is_proposed` = 1;""")
+    try:
+        frappe.db.sql("""UPDATE `tabSalary Slip` SET `is_proposed` = 1;""")
+    except Exception as err:
+        print("Unable to mark old salary slips (probably because patch is run before migration)"
     return
     
     

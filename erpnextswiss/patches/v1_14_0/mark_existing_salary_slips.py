2d962642ea58f8802cca3c451a643ea203e80983
lasalesi <larsericmueller@gmail.com>
2021-11-16 17:29:26 +0100

    hotfix

--- erpnextswiss/patches/v1_14_0/mark_existing_salary_slips.py
+++ erpnextswiss/patches/v1_14_0/mark_existing_salary_slips.py
@@ -6,7 +6,7 @@ def execute():
     try:
         frappe.db.sql("""UPDATE `tabSalary Slip` SET `is_proposed` = 1;""")
     except Exception as err:
-        print("Unable to mark old salary slips (probably because patch is run before migration)"
+        print("Unable to mark old salary slips (probably because patch is run before migration)")
     return
-    
+
     

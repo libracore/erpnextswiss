083dce0ee98270b26bf26451437890e367f09d84
lasalesi <larsericmueller@gmail.com>
2022-01-04 07:05:58 +0100

    adding comments

--- erpnextswiss/erpnextswiss/doctype/payment_proposal/payment_proposal.py
+++ erpnextswiss/erpnextswiss/doctype/payment_proposal/payment_proposal.py
@@ -200,13 +200,15 @@ class PaymentProposal(Document):
     def add_payment(self, receiver_name, iban, payment_type, address_line1, 
         address_line2, country, amount, currency, reference, execution_date, 
         esr_reference=None, esr_participation_number=None, bic=None, is_salary=0):
+            # prepare payment date
             if isinstance(execution_date,datetime):
                 pay_date = execution_date
             else:
                 pay_date = datetime.strptime(execution_date, "%Y-%m-%d")
+            # assure that payment date is not in th past
             if pay_date.date() < datetime.now().date():
                 pay_date = datetime.now().date()
-
+            # append payment record
             new_payment = self.append('payments', {})
             new_payment.receiver = receiver_name
             new_payment.iban = iban

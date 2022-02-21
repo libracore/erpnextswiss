667c4a2553ed0f7d75c8c4a7edcac6eba211a769
Philipp Gruener <philipp.gruener@royal-software.de>
2022-02-21 23:01:57 +0100

    Renamed total_taxes_and_charges in Expense Claim, as this is already present.

--- erpnextswiss/docs/sql/viewVAT_400.sql
+++ erpnextswiss/docs/sql/viewVAT_400.sql
@@ -1,17 +1,17 @@
 CREATE VIEW `viewVAT_400` AS
-SELECT 
-	`tabPurchase Invoice`.`posting_date` AS `posting_date`, 
-    `tabPurchase Invoice`.`name` AS `name`, 
-    `tabPurchase Invoice`.`base_grand_total`  AS `base_grand_total`, 
-    `tabPurchase Invoice`.`taxes_and_charges` AS `taxes_and_charges`, 
+SELECT
+	`tabPurchase Invoice`.`posting_date` AS `posting_date`,
+    `tabPurchase Invoice`.`name` AS `name`,
+    `tabPurchase Invoice`.`base_grand_total`  AS `base_grand_total`,
+    `tabPurchase Invoice`.`taxes_and_charges` AS `taxes_and_charges`,
     `tabPurchase Invoice`.`total_taxes_and_charges` AS `total_taxes_and_charges`
-FROM `tabPurchase Invoice` 
+FROM `tabPurchase Invoice`
 WHERE `docstatus` = 1 AND `taxes_and_charges` LIKE '%400%'
-UNION SELECT 
-	`tabExpense Claim`.`posting_date` AS `posting_date`, 
-    `tabExpense Claim`.`name` AS `name`, 
-    `tabExpense Claim`.`total_claimed_amount`  AS `base_grand_total`, 
-    `tabExpense Claim`.`taxes_and_charges` AS `taxes_and_charges`, 
-    `tabExpense Claim`.`total_taxes_and_charges` AS `total_taxes_and_charges`
+UNION SELECT
+	`tabExpense Claim`.`posting_date` AS `posting_date`,
+    `tabExpense Claim`.`name` AS `name`,
+    `tabExpense Claim`.`total_claimed_amount`  AS `base_grand_total`,
+    `tabExpense Claim`.`taxes_and_charges` AS `taxes_and_charges`,
+    `tabExpense Claim`.`total_taxes_and_charges_enswiss` AS `total_taxes_and_charges_enswiss`
 FROM `tabExpense Claim`
 WHERE `docstatus` = 1 AND `taxes_and_charges` LIKE '%400%'

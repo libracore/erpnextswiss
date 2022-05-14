b0954613805d810524215173e80e00844d40f954
lasalesi <larsericmueller@gmail.com>
2022-05-14 07:30:57 +0200

    multicompany VAT declarations

--- erpnextswiss/erpnextswiss/doctype/vat_declaration/vat_declaration.py
+++ erpnextswiss/erpnextswiss/doctype/vat_declaration/vat_declaration.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017-2020, libracore (https://www.libracore.com) and contributors
+# Copyright (c) 2017-2022, libracore (https://www.libracore.com) and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals

44b31bbefcfe1ad457e2227e674ace4a50d91cea
lasalesi <larsericmueller@gmail.com>
2022-06-17 00:18:42 +0200

    bugfix payment methods

--- erpnextswiss/erpnextswiss/doctype/datatrans_settings/datatrans_settings.py
+++ erpnextswiss/erpnextswiss/doctype/datatrans_settings/datatrans_settings.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class DatatransSettings(Document):
-	pass
+    def get_payment_method_list(self):
+        payment_methods = []
+        for p in self.payment_methods:
+            payment_methods.append(p.code)
+        return payment_methods
+

c4838fe3e954448bceffb850e3b22772b9629c76
latoile <gregory@rodicio.ch>
2023-10-17 15:31:03 +0200

    Bug correction camt_053 details

--- erpnextswiss/erpnextswiss/page/bank_wizard/bank_wizard.py
+++ erpnextswiss/erpnextswiss/page/bank_wizard/bank_wizard.py
@@ -458,12 +458,12 @@ def read_camt_transactions(transaction_entries, account, settings, debug=False):
                         if payment_instruction_id:
                             try:
                                 payment_instruction_fields = payment_instruction_id.split("-")
-                                payment_instruction_row = int(payment_instruction_fields[-1]) + 1
+                                payment_instruction_row = str(payment_instruction_fields[-1]) + str('1')
                                 if len(payment_instruction_fields) > 3:
                                     # revision in payment proposal
                                     payment_proposal_id = "{0}-{1}".format(payment_instruction_fields[1], payment_instruction_fields[2])
                                 else:
-                                    payment_proposal_id = payment_instruction_fields[1]
+                                    payment_proposal_id = payment_instruction_fields[0]
                                 # find original instruction record
                                 payment_proposal_payments = frappe.get_all("Payment Proposal Payment", 
                                     filters={'parent': payment_proposal_id, 'idx': payment_instruction_row},

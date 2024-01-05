8808356bf6d99ca4ce88bcd331acb455a371c86d
lasalesi <larsericmueller@gmail.com>
2024-01-05 16:05:28 +0100

    patch: compensate for missing full name

--- erpnextswiss/erpnextswiss/nextcloud/contacts.py
+++ erpnextswiss/erpnextswiss/nextcloud/contacts.py
@@ -30,7 +30,7 @@ def send_contact_to_nextcloud(contact, event=None, debug=False):
     
     contact_data['uid'] = hashlib.md5((contact.name.lower()).encode("utf-8")).hexdigest()
     if not contact_data.get('full_name'):
-        contact_data['full_name'] = "{0} {2}".format(contact.first_name or "", contact.last_name or "")
+        contact_data['full_name'] = "{0} {1}".format(contact.first_name or "", contact.last_name or "")
         
     if type(contact.modified) == str:
         ts = datetime.strptime(contact.modified[:19], "%Y-%m-%d %H:%M:%S").strftime("%Y%m%dT%H%M%SZ")

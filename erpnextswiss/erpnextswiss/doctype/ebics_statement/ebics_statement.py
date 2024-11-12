063153d5a324876322c2ab628f845da98748e338
lasalesi <larsericmueller@gmail.com>
2024-11-12 22:47:27 +0100

    camt.053 meta information reader

--- erpnextswiss/erpnextswiss/doctype/ebics_statement/ebics_statement.py
+++ erpnextswiss/erpnextswiss/doctype/ebics_statement/ebics_statement.py
@@ -2,9 +2,28 @@
 # Copyright (c) 2024, libracore (https://www.libracore.com) and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnextswiss.erpnextswiss.page.bank_wizard.bank_wizard import read_camt053_meta
 
 class ebicsStatement(Document):
-	pass
+    def parse_content(self):
+        meta = read_camt053_meta(self.xml_content)
+        self.update({
+            'currency': meta.get('currency'),
+            'opening_balance': meta.get('opening_balance'),
+            'closing_balance': meta.get('closing_balance')
+        })
+        account_matches = frappe.db.sql("""
+            SELECT `name`
+            FROM `tabAccount`
+            WHERE `iban` = "{iban}";
+            """.format(iban=meta.get('iban')), as_dict=True)
+            
+        if len(account_matches) > 0:
+            self.account = account_matches[0]['name']
+        
+        self.save()
+        frappe.db.commit()
+        
+        return

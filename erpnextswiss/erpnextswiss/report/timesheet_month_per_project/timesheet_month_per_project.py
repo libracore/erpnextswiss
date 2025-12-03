340da594ca21b5b8f5f47369da7e53f3e1b7d54a
Joel <joel.mesmer@libracore.com>
2025-12-03 17:32:01 +0100

    libracore/swissrecycle#286

--- erpnextswiss/erpnextswiss/report/timesheet_month_per_project/timesheet_month_per_project.py
+++ erpnextswiss/erpnextswiss/report/timesheet_month_per_project/timesheet_month_per_project.py
@@ -9,12 +9,22 @@ def execute(filters=None):
     
     columns = ["Project::200", "Hours"]
     if filters:
-        data = frappe.db.sql("""SELECT `project` AS `Project`, SUM(`hours`) AS `Hours`
-            FROM `tabTimesheet Detail` 
-            LEFT JOIN `tabTimesheet` ON `tabTimesheet Detail`.`parent` = `tabTimesheet`.`name`
-            WHERE `tabTimesheet`.`docstatus` = 1 AND `from_time` > "{0} 0:0" AND `from_time` < "{1} 23:59"
-            GROUP BY `Project`
-            ORDER BY `Hours` DESC""".format(filters.from_date, filters.end_date), as_list = True)
+        if filters.employee:
+            columns = ["Project::200", "Hours"]
+            data = frappe.db.sql("""SELECT `project` AS `Project`, SUM(`hours`) AS `Hours`
+                FROM `tabTimesheet Detail` 
+                LEFT JOIN `tabTimesheet` ON `tabTimesheet Detail`.`parent` = `tabTimesheet`.`name`
+                WHERE `tabTimesheet`.`docstatus` = 1 AND `from_time` > "{0} 0:0" AND `from_time` < "{1} 23:59"
+                AND `tabTimesheet`.`employee` = "{2}"
+                GROUP BY `Project`
+                ORDER BY `Hours` DESC""".format(filters.from_date, filters.end_date, filters.employee), as_list = True)
+        else:
+            data = frappe.db.sql("""SELECT `project` AS `Project`, SUM(`hours`) AS `Hours`
+                FROM `tabTimesheet Detail` 
+                LEFT JOIN `tabTimesheet` ON `tabTimesheet Detail`.`parent` = `tabTimesheet`.`name`
+                WHERE `tabTimesheet`.`docstatus` = 1 AND `from_time` > "{0} 0:0" AND `from_time` < "{1} 23:59"
+                GROUP BY `Project`
+                ORDER BY `Hours` DESC""".format(filters.from_date, filters.end_date), as_list = True)
     else:
         data = frappe.db.sql("""SELECT `project` AS `Project`, SUM(`hours`) AS `Hours`
             FROM `tabTimesheet Detail` 

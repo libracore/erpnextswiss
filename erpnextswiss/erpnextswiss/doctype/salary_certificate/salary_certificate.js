c50df37c45e63dd7a9857b08af052d294cd847b0
Philipp Benkert <philipp.benkert@gmail.com>
2026-05-16 08:06:10 +0200

    Fix Swiss salary certificate UI

--- erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.js
+++ erpnextswiss/erpnextswiss/doctype/salary_certificate/salary_certificate.js
@@ -3,10 +3,25 @@
 
 frappe.ui.form.on('Salary Certificate', {
 	refresh: function(frm) {
-        // add utility buttons
-        frm.add_custom_button(__("Get values"), function() {
-            if (frm.doc.name.startsWith("New")) {
-                frappe.msgprint( __("Please save the record before populating it with data") );
+        if (!frm.is_new()) {
+            frm.add_custom_button(__("Drucken"), function() {
+                frappe.set_route("print", frm.doctype, frm.docname);
+            }, __("Ausgabe"));
+
+            frm.add_custom_button(__("PDF"), function() {
+                var print_format = frm.meta.default_print_format || "605.040.18N Form. 11";
+                var url = "/api/method/frappe.utils.print_format.download_pdf"
+                    + "?doctype=" + encodeURIComponent(frm.doctype)
+                    + "&name=" + encodeURIComponent(frm.docname)
+                    + "&format=" + encodeURIComponent(print_format)
+                    + "&no_letterhead=1";
+                window.open(url, "_blank");
+            }, __("Ausgabe"));
+        }
+
+        frm.add_custom_button(__("Werte holen"), function() {
+            if (frm.is_new()) {
+                frappe.msgprint(__("Bitte speichern Sie den Lohnausweis, bevor Werte geholt werden."));
             } else {
                 frappe.call({
                     method: 'fetch_values',
@@ -15,12 +30,12 @@ frappe.ui.form.on('Salary Certificate', {
                         frm.refresh();
                                     
                         // show a short information
-                        show_alert( __("Salary slip information collected"));
+                        show_alert(__("Lohnwerte wurden geholt."));
                     }
                 }); 
             }
         });
-        frm.add_custom_button(__("Calculate totals"), function() {
+        frm.add_custom_button(__("Summen berechnen"), function() {
             // calculate gross salary
             var gross_salary = frm.doc.salary
                 + frm.doc.catering
@@ -43,7 +58,7 @@ frappe.ui.form.on('Salary Certificate', {
             cur_frm.set_value('net_salary', net_salary);    
             
             // show a short information
-            show_alert( __("Totals have been updated"));
+            show_alert(__("Summen wurden aktualisiert."));
         });
 	}
 });

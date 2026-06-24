a290bb0f50c2bfbb047c78cea3960310fca63187
zwieble <anja.friedrich@libracore.com>
2026-06-24 11:47:20 +0200

    add abaninja settings

---
+++ erpnextswiss/erpnextswiss/doctype/abaninja_settings/abaninja_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, libracore (https://www.libracore.com) and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AbaNinja Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });

00fcb230b8b7372db3b06e56092eb1fad7b7cefb
Lars <lars.mueller@libracore.com>
2026-07-15 14:28:42 +0000

    various improvements for more robust payment proposal creation (show all errors, correct IBAN case, check QR-IBAN vs IBAN)
    (cherry picked from commit 1c4e11180092e18d074bd710e31f6cf3e1c82d94)
    
    Co-authored-by: lasalesi <larsericmueller@gmail.com>

--- erpnextswiss/public/js/supplier.js
+++ erpnextswiss/public/js/supplier.js
@@ -14,9 +14,36 @@ frappe.ui.form.on('Supplier', {
         }
     },
     before_save(frm) {
+        // prevent lower case characters in IBANs
+        if (frm.doc.iban) {
+            cur_frm.set_value("iban", frm.doc.iban.toUpperCase());
+        }
+        if (frm.doc.esr_participation_number) {
+            cur_frm.set_value("esr_participation_number", frm.doc.esr_participation_number.toUpperCase());
+        }
+        
+        // verify IBAN vs. ESR-/QR-IBAN
+        if ((frm.doc.iban) 
+            && (frm.doc.iban.startsWith("CH"))
+            && (frm.doc.iban.replaceAll(" ", "")[4] === "3")) {
+                frappe.msgprint( __("The provided IBAN is a QR-IBAN, moving it to QR-IBAN field"), __("Notification") );
+                cur_frm.set_value("esr_participation_number", frm.doc.iban);
+                cur_frm.set_value("iban", "");
+        }
+        if ((frm.doc.esr_participation_number) 
+            && (frm.doc.esr_participation_number.startsWith("CH")) 
+            && (frm.doc.esr_participation_number.replaceAll(" ", "")[4] !== "3")) {
+                frappe.msgprint( __("The provided QR-IBAN is an IBAN, moving it to IBAN field"), __("Notification") );
+                cur_frm.set_value("iban", frm.doc.esr_participation_number);
+                cur_frm.set_value("esr_participation_number", "");
+        }
+        
         if ((frm.doc.default_payment_method === "IBAN") && (!frm.doc.iban) && (frm.doc.esr_participation_number)) {
             // default to ESR if ESR number is available but IBAN is not
             cur_frm.set_value("default_payment_method", "ESR");
+        } else if ((frm.doc.default_payment_method === "ESR") && (!frm.doc.esr_participation_number) && (frm.doc.iban)) {
+            // default to ESR if ESR number is available but IBAN is not
+            cur_frm.set_value("default_payment_method", "IBAN");
         }
     }
 });
